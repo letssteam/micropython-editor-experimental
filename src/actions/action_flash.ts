1a1a482ebdaec99044ad60cee90cd70010f4122a
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-03-14 19:03:32 +0100

    Start implement dialog

--- src/actions/action_flash.ts
+++ src/actions/action_flash.ts
@@ -13,14 +13,19 @@ export class ActionFlash implements Action {
     private serial_ouput: SerialOutput;
 
     private dialog: HTMLElement;
+    private progress_bar_div: HTMLElement;
 
-    constructor(daplink: DapLinkWrapper, serial_output: SerialOutput, getData: GetDataCallback){
+    constructor(dialog_parent: HTMLElement, daplink: DapLinkWrapper, serial_output: SerialOutput, getData: GetDataCallback){
         this.getData_cb = getData;
         this.daplink = daplink;
         this.serial_ouput = serial_output;
+
+        this.createDialog(dialog_parent);
     }
 
     async run() : Promise<boolean>{
+
+        this.openDialog();
 
         if( this.daplink.isConnected() )
         {
@@ -66,4 +71,73 @@ export class ActionFlash implements Action {
         let rep = await fetch(file);
         return await rep.arrayBuffer();
     }
+
+    private createDialog(parent: HTMLElement){
+        this.dialog = document.createElement("div");
+        this.dialog.classList.add("progress-dialog");
+        this.dialog.style.display = "none";
+
+        let container = document.createElement("div");
+        container.classList.add("progress-container")
+
+        let title = document.createElement("div");
+        title.classList.add("progress-content-title");
+        title.innerText = "Uploading...";
+
+        let content = document.createElement("div");
+        content.classList.add("progress-content");
+
+        let text = document.createElement("p");
+        text.innerHTML = `
+            Your program is uploading to your target, please wait.<br/>
+            <br/>
+            <i>Do not unplugged your board, do not close this tab nor change tab during uploading.</i>
+        `;
+
+        let close_button = document.createElement("button");
+        close_button.classList.add("progress-bar-close-button");
+        close_button.innerText = "Close";
+        close_button.addEventListener( "click", () => this.closeDialog() );
+        
+        this.progress_bar_div = document.createElement("div");
+        this.progress_bar_div.classList.add("progress-bar-container")
+
+        let value = document.createElement("p");
+        value.classList.add("progress-bar-value");
+
+        let bar = document.createElement("div");
+        bar.classList.add("progress-bar-cursor");
+
+        this.progress_bar_div.append(value);
+        this.progress_bar_div.append(bar);
+
+
+        content.append(text);
+        content.append(this.progress_bar_div);
+        content.append(close_button);
+
+        container.append(title);
+        container.append(content);
+
+        this.dialog.append(container);
+
+        parent.append(this.dialog);
+    }
+
+    private openDialog(){
+        this.dialog.style.display = "block";
+
+        this.setProgressBarValue(33);
+        (this.progress_bar_div.querySelector(".progress-bar-close-button") as HTMLElement).style.display = "none";
+    }
+
+    private setProgressBarValue(value: number){
+        (this.progress_bar_div.querySelector(".progress-bar-value") as HTMLElement).innerHTML = value + "%";
+        (this.progress_bar_div.querySelector(".progress-bar-cursor") as HTMLElement).style.width = value + "%";
+    }
+
+    private closeDialog(){
+        this.dialog.style.display = "none";
+    }
+
 }
